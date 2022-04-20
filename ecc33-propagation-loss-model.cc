75aea5b83379cb039d61ff5c45b4daf9997ee4bf
unknown <vwang314@gmail.com>
2022-04-19 22:47:58 -0400

    implement ecc33 model

--- ecc33-propagation-loss-model.cc
+++ ecc33-propagation-loss-model.cc
@@ -1,6 +1,5 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2007,2008, 2009 INRIA, UDcast
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,167 +14,157 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Mohamed Amine Ismail <amine.ismail@sophia.inria.fr>
- *                              <amine.ismail@udcast.com>
  */
+
+ /*
+  * ECC-33 Propagation Path Loss Model
+  * A_fs - free space attenuation (dB)
+  * A_bm - basic medium path loss (dB)
+  * G_b  - base station/transmitter antenna height gain factor
+  * G_r  - receiver antenna height gain factor
+  *  L   - path loss (dB)
+  *  f   - frequency (GhZ)
+  *  d   - distance between base station (transmitter) and mobile antenna (receiver) (km)
+  * h_b  - base station (transmitter) height (m)
+  * h_r  - mobile antenna (receiver) height (m)
+  *
+  *  L   = A_fs + A_bm - G_b - G_r
+  * A_fs = 92.4 + 20log(d) + log(f)
+  * A_bm = 20.41 + 9.83log(d) + 7.894log(f) + 9.56[log(f)]^2
+  * G_b  = log(h_b/200)(13.958 + 5.8log(d))^2
+  * G_r suburban = [42.57 + 13.7log(f)][log(h_r)-0.585]
+  * G_r urban    = 0.759h_r - 1.862
+  */
 
 #include "ns3/propagation-loss-model.h"
 #include "ns3/log.h"
 #include "ns3/mobility-model.h"
 #include "ns3/double.h"
+#include "ns3/enum.h"
 #include "ns3/pointer.h"
 #include <cmath>
-#include "cost231-propagation-loss-model.h"
+#include "ecc33-propagation-loss-model.h"
 
 namespace ns3 {
 
-NS_LOG_COMPONENT_DEFINE ("Cost231PropagationLossModel");
+NS_LOG_COMPONENT_DEFINE ("ECC33PropagationLossModel");
 
-NS_OBJECT_ENSURE_REGISTERED (Cost231PropagationLossModel);
+NS_OBJECT_ENSURE_REGISTERED (ECC33PropagationLossModel);
 
 TypeId
-Cost231PropagationLossModel::GetTypeId (void)
+ECC33PropagationLossModel::GetTypeId (void)
 {
-  static TypeId tid = TypeId ("ns3::Cost231PropagationLossModel")
+  static TypeId tid = TypeId ("ns3::ECC33PropagationLossModel")
     .SetParent<PropagationLossModel> ()
     .SetGroupName ("Propagation")
-    .AddConstructor<Cost231PropagationLossModel> ()
-    .AddAttribute ("Lambda",
-                   "The wavelength  (default is 2.3 GHz at 300 000 km/s).",
-                   DoubleValue (300000000.0 / 2.3e9),
-                   MakeDoubleAccessor (&Cost231PropagationLossModel::m_lambda),
-                   MakeDoubleChecker<double> ())
+    .AddConstructor<ECC33PropagationLossModel> ()
     .AddAttribute ("Frequency",
-                   "The Frequency  (default is 2.3 GHz).",
-                   DoubleValue (2.3e9),
-                   MakeDoubleAccessor (&Cost231PropagationLossModel::m_frequency),
+                   "The Frequency  (default is 2 GHz).",
+                   DoubleValue (2e9),
+                   MakeDoubleAccessor (&ECC33PropagationLossModel::m_frequency),
                    MakeDoubleChecker<double> ())
-    .AddAttribute ("BSAntennaHeight",
-                   "BS Antenna Height (default is 50m).",
+    .AddAttribute ("TxAntennaHeight",
+                   "Tx Antenna Height (default is 50m).",
                    DoubleValue (50.0),
-                   MakeDoubleAccessor (&Cost231PropagationLossModel::m_BSAntennaHeight),
+                   MakeDoubleAccessor (&ECC33PropagationLossModel::m_TxAntennaHeight),
                    MakeDoubleChecker<double> ())
-    .AddAttribute ("SSAntennaHeight",
-                   "SS Antenna Height (default is 3m).",
+    .AddAttribute ("RxAntennaHeight",
+                   "Rx Antenna Height (default is 3m).",
                    DoubleValue (3),
-                   MakeDoubleAccessor (&Cost231PropagationLossModel::m_SSAntennaHeight),
+                   MakeDoubleAccessor (&ECC33PropagationLossModel::m_RxAntennaHeight),
                    MakeDoubleChecker<double> ())
-    .AddAttribute ("MinDistance",
-                   "The distance under which the propagation model refuses to give results (m) ",
-                   DoubleValue (0.5),
-                   MakeDoubleAccessor (&Cost231PropagationLossModel::SetMinDistance, &Cost231PropagationLossModel::GetMinDistance),
-                   MakeDoubleChecker<double> ());
+    .AddAttribute ("Environment",
+                   "Type of environment (default is urban) ",
+                   EnumValue (Urban),
+                   MakeEnumAccessor (&ECC33PropagationLossModel::m_environment),
+                   MakeEnumChecker (Urban, "Urban", Suburban, "Suburban"));
   return tid;
 }
 
-Cost231PropagationLossModel::Cost231PropagationLossModel ()
+ECC33PropagationLossModel::ECC33PropagationLossModel ()
 {
-  m_shadowing = 10;
 }
 
 void
-Cost231PropagationLossModel::SetLambda (double frequency, double speed)
+ECC33PropagationLossModel::SetFrequency (double frequency)
 {
-  m_lambda = speed / frequency;
   m_frequency = frequency;
 }
 
 double
-Cost231PropagationLossModel::GetShadowing (void)
+ECC33PropagationLossModel::GetFrequency (void) const
 {
-  return m_shadowing;
-}
-void
-Cost231PropagationLossModel::SetShadowing (double shadowing)
-{
-  m_shadowing = shadowing;
+  return m_frequency;
 }
 
 void
-Cost231PropagationLossModel::SetLambda (double lambda)
+ECC33PropagationLossModel::SetTxAntennaHeight (double height)
 {
-  m_lambda = lambda;
-  m_frequency = 300000000 / lambda;
+  m_TxAntennaHeight = height;
 }
 
 double
-Cost231PropagationLossModel::GetLambda (void) const
+ECC33PropagationLossModel::GetTxAntennaHeight (void) const
 {
-  return m_lambda;
+  return m_TxAntennaHeight;
 }
 
 void
-Cost231PropagationLossModel::SetMinDistance (double minDistance)
+ECC33PropagationLossModel::SetRxAntennaHeight (double height)
 {
-  m_minDistance = minDistance;
+  m_RxAntennaHeight = height;
 }
+
 double
-Cost231PropagationLossModel::GetMinDistance (void) const
+ECC33PropagationLossModel::GetRxAntennaHeight (void) const
 {
-  return m_minDistance;
+  return m_RxAntennaHeight;
 }
 
 void
-Cost231PropagationLossModel::SetBSAntennaHeight (double height)
+ECC33PropagationLossModel::SetEnvironment (Environment environment)
 {
-  m_BSAntennaHeight = height;
+  m_environment = environment;
+}
+
+ECC33PropagationLossModel::Environment
+ECC33PropagationLossModel::GetEnvironment (void) const
+{
+  return m_environment;
 }
 
 double
-Cost231PropagationLossModel::GetBSAntennaHeight (void) const
+ECC33PropagationLossModel::GetLoss (Ptr<MobilityModel> a, Ptr<MobilityModel> b) const
 {
-  return m_BSAntennaHeight;
-}
-
-void
-Cost231PropagationLossModel::SetSSAntennaHeight (double height)
-{
-  m_SSAntennaHeight = height;
-}
-
-double
-Cost231PropagationLossModel::GetSSAntennaHeight (void) const
-{
-  return m_SSAntennaHeight;
-}
-
-double
-Cost231PropagationLossModel::GetLoss (Ptr<MobilityModel> a, Ptr<MobilityModel> b) const
-{
-
   double distance = a->GetDistanceFrom (b);
-  if (distance <= m_minDistance)
-    {
-      return 0.0;
-    }
+  distance = distance / 1000;
+  double frequency = m_frequency/1e9;
 
-  double frequency_MHz = m_frequency * 1e-6;
+  double A_fs = 92.4 + 20*std::log10(distance) + 20*std::log10(frequency);
+  double A_bm = 20.41 + 9.83*std::log10(distance) + 7.89*std::log10(frequency) + 9.56*pow( std::log10(frequency), 2);
+  double G_b = std::log10(m_TxAntennaHeight/200)*pow( (13.958 + 5.8*std::log10(distance)), 2);
+  double G_r = 0.0;
+  if (m_environment == Urban){
+    G_r = 0.759*m_RxAntennaHeight - 1.862;
+  } else {
+    G_r = (42.57 + 13.7*std::log10(frequency))*(std::log10(m_RxAntennaHeight) - 0.585);
+  }
 
-  double distance_km = distance * 1e-3;
-
-  double C_H = 0.8 + ((1.11 * std::log10(frequency_MHz)) - 0.7) * m_SSAntennaHeight - (1.56 * std::log10(frequency_MHz));
-
-  // from the COST231 wiki entry
-  // See also http://www.lx.it.pt/cost231/final_report.htm
-  // Ch. 4, eq. 4.4.3, pg. 135
-
-  double loss_in_db = 46.3 + (33.9 * std::log10(frequency_MHz)) - (13.82 * std::log10 (m_BSAntennaHeight)) - C_H
-		  	  	  + ((44.9 - 6.55 * std::log10 (m_BSAntennaHeight)) * std::log10 (distance_km)) + m_shadowing;
+  double loss_in_db = A_fs + A_bm - G_b - G_r;
 
   NS_LOG_DEBUG ("dist =" << distance << ", Path Loss = " << loss_in_db);
 
   return (0 - loss_in_db);
-
 }
 
 double
-Cost231PropagationLossModel::DoCalcRxPower (double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b) const
+ECC33PropagationLossModel::DoCalcRxPower (double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b) const
 {
   return txPowerDbm + GetLoss (a, b);
 }
 
 int64_t
-Cost231PropagationLossModel::DoAssignStreams (int64_t stream)
+ECC33PropagationLossModel::DoAssignStreams (int64_t stream)
 {
   return 0;
 }
--- ecc33-propagation-loss-model.h
+++ ecc33-propagation-loss-model.h
@@ -1,6 +1,5 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2007,2008, 2009 INRIA, UDcast
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,39 +14,17 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Mohamed Amine Ismail <amine.ismail@sophia.inria.fr>
- *                              <amine.ismail@udcast.com>
  */
 
-#ifndef COST231_PROPAGATION_LOSS_MODEL_H
-#define COST231_PROPAGATION_LOSS_MODEL_H
+#ifndef ECC33_PROPAGATION_LOSS_MODEL_H
+#define ECC33_PROPAGATION_LOSS_MODEL_H
 
 #include "ns3/nstime.h"
 #include "ns3/propagation-loss-model.h"
 
 namespace ns3 {
 
-/**
- * \ingroup propagation
- *
- *  \brief The COST-Hata-Model is the most often cited of the COST 231 models.
- *
- *  Also called the Hata Model PCS Extension, it is a radio propagation model
- *  that extends the Hata Model (which in turn is based on the Okumura Model)
- *  to cover a more elaborated range of frequencies. COST (COperation
- *  europ�enne dans le domaine de la recherche Scientifique et Technique)
- *  is a European Union Forum for cooperative scientific research which has
- *  developed this model accordingly to various experiments and researches.
- *  This model is applicable to urban areas. To further evaluate Path Loss
- *  in Suburban or Rural Quasi-open/Open Areas.
- *  Frequency: 1500 MHz to 2000 MHz
- *  Mobile Station Antenna Height: 1 up to 10m
- *  Base station Antenna Height: 30m to 200m
- *  Link Distance:up to 20 km
- *
- */
-
-class Cost231PropagationLossModel : public PropagationLossModel
+class ECC33PropagationLossModel : public PropagationLossModel
 {
 
 public:
@@ -56,7 +33,11 @@ public:
    * \return the object TypeId
    */
   static TypeId GetTypeId (void);
-  Cost231PropagationLossModel ();
+  ECC33PropagationLossModel ();
+  enum Environment
+  {
+    Suburban, Urban
+  };
 
   /**
    * Get the propagation loss
@@ -67,88 +48,71 @@ public:
   double GetLoss (Ptr<MobilityModel> a, Ptr<MobilityModel> b) const;
 
   /**
-   * Set the BS antenna height
-   * \param height BS antenna height [m]
+   * Set the frequency
+   * \param frequency frequency [Hz]
    */
-  void SetBSAntennaHeight (double height);
+  void SetFrequency (double frequency);
   /**
-   * Set the SS antenna height
-   * \param height SS antenna height [m]
+   * Get the frequency
+   * \returns frequency [Hz]
    */
-  void SetSSAntennaHeight (double height);
+  double GetFrequency (void) const;
+  /**
+   * Set the Tx antenna height
+   * \param height Tx antenna height [m]
+   */
+  void SetTxAntennaHeight (double height);
+  /**
+   * Get the Tx antenna height
+   * \returns Tx antenna height [m]
+   */
+  double GetTxAntennaHeight (void) const;
+  /**
+   * Set the RX antenna height
+   * \param height RX antenna height [m]
+   */
+  void SetRxAntennaHeight (double height);
+  /**
+   * Get the RX antenna height
+   * \returns RX antenna height [m]
+   */
+  double GetRxAntennaHeight (void) const;
+  /**
+   * Set the environment
+   * \param environment
+   */
+  void SetEnvironment (Environment environment);
+  /**
+   * Get the environment
+   * \returns environment
+   */
+  Environment GetEnvironment (void) const;
 
-  /**
-   * Set the wavelength
-   * \param lambda the wavelength
-   */
-  void SetLambda (double lambda);
-  /**
-   * Set the wavelength
-   * \param frequency the signal frequency [Hz]
-   * \param speed the signal speed [m/s]
-   */
-  void SetLambda (double frequency, double speed);
-  /**
-   * Set the minimum model distance
-   * \param minDistance the minimum model distance
-   */
-  void SetMinDistance (double minDistance);
-  /**
-   * Get the BS antenna height
-   * \returns BS antenna height [m]
-   */
-  double GetBSAntennaHeight (void) const;
-  /**
-   * Get the SS antenna height
-   * \returns SS antenna height [m]
-   */
-  double GetSSAntennaHeight (void) const;
-  /**
-   * Get the minimum model distance
-   * \returns the minimum model distance
-   */
-  double GetMinDistance (void) const;
-  /**
-   * Get the wavelength
-   * \returns the wavelength
-   */
-  double GetLambda (void) const;
-  /**
-   * Get the shadowing value
-   * \returns the shadowing value
-   */
-  double GetShadowing (void);
-  /**
-   * Set the shadowing value
-   * \param shadowing the shadowing value
-   */
-  void SetShadowing (double shadowing);
 private:
   /**
    * \brief Copy constructor
    *
    * Defined and unimplemented to avoid misuse
    */
-  Cost231PropagationLossModel (const Cost231PropagationLossModel &);
+  ECC33PropagationLossModel (const ECC33PropagationLossModel &);
   /**
    * \brief Copy constructor
    *
    * Defined and unimplemented to avoid misuse
    * \returns
    */
-  Cost231PropagationLossModel & operator = (const Cost231PropagationLossModel &);
+  ECC33PropagationLossModel & operator = (const ECC33PropagationLossModel &);
 
   virtual double DoCalcRxPower (double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b) const;
   virtual int64_t DoAssignStreams (int64_t stream);
-  double m_BSAntennaHeight; //!< BS Antenna Height [m]
-  double m_SSAntennaHeight; //!< SS Antenna Height [m]
-  double m_lambda; //!< The wavelength
-  double m_minDistance; //!< minimum distance [m]
+
   double m_frequency; //!< frequency [Hz]
-  double m_shadowing; //!< Shadowing loss [dB]
+  double m_TxAntennaHeight; //!< Tx Antenna Height [m]
+  double m_RxAntennaHeight; //!< Rx Antenna Height [m]
+  Environment m_environment;
 
 };
 
 }
 
-#endif /* COST231PROPAGATIONMODEL_H */
+#endif /* ECC33PROPAGATIONMODEL_H */
