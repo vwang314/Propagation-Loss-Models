dd84fa3cd60c9c3389be4e9d9e66f330e81109d4
unknown <vwang314@gmail.com>
2022-05-04 01:29:35 -0400

    update scratch file

--- scratch/empirical-propagation-loss-comparison.cc
+++ scratch/empirical-propagation-loss-comparison.cc
@@ -40,61 +40,70 @@
 #include "ns3/cost231-propagation-loss-model.h"
 #include "ns3/ecc33-propagation-loss-model.h"
 #include "ns3/ericsson-propagation-loss-model.h"
+#include "ns3/sui-propagation-loss-model.h"
 
 using namespace ns3;
 using namespace std;
 
 NS_LOG_COMPONENT_DEFINE ("EmpiricalPropagationLossComparison");
 
+double urban[20][2] = { {80, 0}, {100, 0}, {150, 0}, {200, 0}, {250, 0}, {300, 0}, {350, 0}, {400, 0}, {450, 0}, {500, 0}, {550, 0}, {600, 0}, {650, 0}, {700, 0}, {750, 0}, {790, 0}, {790, 50}, {790, 100}, {790, 150}, {790, 150} };
+double suburban[20][2] = { {56.57, 56.57}, {100, 100}, {125, 125}, {150, 150}, {150, 200}, {150, 250}, {150, 300}, {200, 300}, {250, 300},  {300, 300}, {300, 350}, {300, 400}, {350, 400}, {400, 400}, {400, 450},  {400, 500}, {400, 550}, {400, 600}, {400, 650}, {400, 700} };
+double rural[20][2] = { {0, 80}, {0, 125}, {50, 125}, {60, 150}, {70, 200}, {100, 230}, {150, 250}, {200, 300}, {225, 315}, {250, 330}, {275, 335}, {300, 340}, {350, 370}, {400, 380}, {450, 390}, {500, 400}, {550, 400}, {600, 400}, {650, 400}, {700, 400} };
+
 void
-AdvancePosition (Ptr<Node> tx, Ptr<Node> rx, vector<Ptr<PropagationLossModel>>  models, double stepsSize, double stepsTime, vector<Gnuplot2dDataset> output)
+AdvancePosition (Ptr<Node> tx, Ptr<Node> rx, vector<Ptr<PropagationLossModel>>  models, int idx, double path[20][2], vector<Gnuplot2dDataset> output)
 {
   Ptr<MobilityModel> tx_mobility = tx->GetObject<MobilityModel> ();
   Ptr<MobilityModel> rx_mobility = rx->GetObject<MobilityModel> ();
   Vector pos = rx_mobility->GetPosition ();
   double dist = CalculateDistance(pos, tx_mobility->GetPosition());
-  // cout << dist;
   for(uint16_t i = 0; i < models.size(); i++){
-    // cout << ", " << 47 - models.at(i)->CalcRxPower (47, tx_mobility, rx_mobility);
     output.at(i).Add(dist, 47 - models.at(i)->CalcRxPower (47, tx_mobility, rx_mobility));
+    //output.at(i).Add(pos.x, pos.y, 47 - models.at(i)->CalcRxPower (47, tx_mobility, rx_mobility));
   }
-  // cout << endl;
-  pos.x += stepsSize;
+  pos.x = path[idx][0];
+  pos.y = path[idx][1];
   rx_mobility->SetPosition (pos);
-  Simulator::Schedule (Seconds (stepsTime), &AdvancePosition, tx, rx, models, stepsSize, stepsTime, output);
+  Simulator::Schedule (Seconds (1), &AdvancePosition, tx, rx, models, idx+1, path, output);
 }
 
 int main (int argc, char *argv[])
 {
   double ap1_z = 33.0;
   double sta1_z = 1.0;
-  int steps = 75;
-  double stepsSize = 10.0;
-  double stepsTime = 1.0;
   double frequency = 900e6; 
   string env = "urban";
+  double path[20][2]; 
 
   CommandLine cmd (__FILE__);
-  cmd.AddValue ("steps", "How many different distances to try", steps);
-  cmd.AddValue ("stepsTime", "Time on each step", stepsTime);
-  cmd.AddValue ("stepsSize", "Distance between steps", stepsSize);
   cmd.AddValue ("environment", "Environment type", env);
   cmd.Parse (argc, argv);
 
-  // LogComponentEnable ("ECC33PropagationLossModel", LOG_LEVEL_INFO);
-
   if(env == "urban"){
     ap1_z = 33.0;
+    for(int i=0; i < 20; i++){
+      path[i][0] = urban[i][0];
+      path[i][1] = urban[i][1];
+    }
   } else if (env == "suburban") {
     ap1_z = 35.0;
+    for(int i=0; i < 20; i++){
+      path[i][0] = suburban[i][0];
+      path[i][1] = suburban[i][1];
+    }
   } else if (env == "rural") {
     ap1_z = 42.0;
+    for(int i=0; i < 20; i++){
+      path[i][0] = rural[i][0];
+      path[i][1] = rural[i][1];
+    }
   } else {
     cout << "Invaid environment type. Please enter 'urban', 'suburban', or 'rural.'" << endl;
     return 1;
   }
 
-  double simuTime = steps * stepsTime;
+  double simuTime = 20;
 
   // Define the APs
   NodeContainer wifiApNodes;
@@ -171,6 +180,19 @@ int main (int argc, char *argv[])
   output.push_back(Gnuplot2dDataset("Cost-231"));
 
   // SUI
+  Ptr<SUIPropagationLossModel> sui = CreateObject<SUIPropagationLossModel> ();
+  sui->SetLambda(3e8/frequency);
+  sui->SetBSAntennaHeight(ap1_z);
+  sui->SetSSAntennaHeight(sta1_z);
+  if(env == "urban"){
+    sui->SetTerrain(ns3::SUIPropagationLossModel::A);
+  } else if (env == "suburban"){
+    sui->SetTerrain(ns3::SUIPropagationLossModel::B);
+  } else if (env == "rural"){
+    sui->SetTerrain(ns3::SUIPropagationLossModel::C);
+  }
+  models.push_back(sui);
+  output.push_back(Gnuplot2dDataset("SUI"));
 
   // ECC-33
   Ptr<ECC33PropagationLossModel> ecc33 = CreateObject<ECC33PropagationLossModel> ();
@@ -188,7 +210,7 @@ int main (int argc, char *argv[])
   }
 
 
-  Simulator::Schedule (Seconds (0.5 + stepsTime), &AdvancePosition, wifiApNodes.Get (0), wifiStaNodes.Get (0), models, stepsSize, stepsTime, output);
+  Simulator::Schedule (Seconds (0.5 + 1), &AdvancePosition, wifiApNodes.Get (0), wifiStaNodes.Get (0), models, 0, path, output);
 
   Simulator::Stop (Seconds (simuTime));
   Simulator::Run ();
@@ -198,9 +220,18 @@ int main (int argc, char *argv[])
   gnuplot.SetTerminal ("png");
   gnuplot.SetLegend ("Distance (m)", "Propagation Loss (dB)");
   env[0] = toupper(env[0]);
-  gnuplot.SetTitle (env + " Propagation Loss vs Distance");
+  gnuplot.SetTitle (env + " Propagation Loss vs Path");
+  // gnuplot.AppendExtra ("set xlabel 'X Pos (m)' offset 0,0,-5");
+  // gnuplot.AppendExtra ("set ylabel 'Y Pos (m)' offset 0,0,0");
+  gnuplot.AppendExtra ("set zlabel 'Path loss (dB)' offset 0,0,0");
+  gnuplot.AppendExtra ("set xrange [100:+800]");
+  gnuplot.AppendExtra ("set yrange [20:+200]");
+  //gnuplot.AppendExtra ("set zrange [0:+200]");
+  //gnuplot.AppendExtra ("set ticslevel 0");
+
   for(uint16_t i = 0; i < output.size(); i++){
-    output.at(i).SetStyle(Gnuplot2dDataset::Style::LINES);
+    //output.at(i).SetStyle("with boxes");
+    output.at(i).SetStyle(ns3::Gnuplot2dDataset::Style::LINES);
     gnuplot.AddDataset (output.at(i));
   }
   gnuplot.GenerateOutput (outfile);
